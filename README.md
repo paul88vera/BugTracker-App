82f5a892ca29d90846633af772864402888ed526
paul88vera <paul88vera@gmail.com>
2021-11-09 16:53:52 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # BugTracker-App
+
+![Screenshot](./images/Homepage-Screenshot.png)
+
+![Screenshot](./images/EntryForm-Screenshot.png)
+
+## Purpose
+The goal of this project was to use HTML, CSS, and JavaScript to create a functional and great looking online application.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website
+https://paul88vera.github.io/BugTracker-App/
+
+## Contribution
+Made by [Paul Vera]
+
+### ©2021 Verafied Creations
---
+++ assets/bugTracker.js
@@ -0,0 +1,32 @@
+// Variables
+var title = document.getElementById("title");
+var issue = document.getElementById("issue");
+var assigned = document.getElementById("assigned");
+var startTime = document.getElementById("startTime");
+var dueDate = document.getElementById("dueDate");
+var priority = document.getElementById("priority");
+var timer = document.getElementById("timer");
+var progress = document.getElementById("progress");
+//Pop-up screen for entries
+function pop(){
+    var popUp = document.getElementById("popUp");  
+        popUp.style.display = "block";
+}
+//Closing & Deleting new entries
+function cls(){
+    var popUp = document.getElementById("popUp"); 
+    popUp.style.display = "none";
+}
+//Title Entry Function
+function title(){
+    var x = document.getElementById("titleEntry").innerHTML;
+    document.getElementById("popTitle").innerHTML = x;
+}
+//New Entry function
+const addNewEntry = document.querySelector("new-call");
+addNewEntry.addEventListener("click", newEntry());
+function newEntry(){
+   const newTr = document.createElement("table-row");
+   newTr.classList.add('entry');
+   document.getElementById("titleBar").appendChild(newTr);
+}
