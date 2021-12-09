c7de49a30daac08d150e12c852b97503dc46ceac
paul88vera <paul88vera@gmail.com>
2021-12-08 23:50:32 -0600

    initial datepicker added

--- assets/bugTracker.js
+++ assets/bugTracker.js
@@ -1,32 +1,41 @@
-// Variables
-var title = document.getElementById("title");
-var issue = document.getElementById("issue");
-var assigned = document.getElementById("assigned");
-var startTime = document.getElementById("startTime");
-var dueDate = document.getElementById("dueDate");
-var priority = document.getElementById("priority");
-var timer = document.getElementById("timer");
-var progress = document.getElementById("progress");
-//Pop-up screen for entries
-function pop() {
-  var popUp = document.getElementById("popUp");
+////  popUp function that shows entry window
+// TODO: delete button in popUp window needs to delete single array data
+// TODO: submit button adds data to entry as single object
+// TODO: create selectable assignee's and assigners
+// TODO: entries listed as object data
+// TODO: add entries to local storage
+
+var addEntry = [];
+
+/* OPENS ENTRY WINDOW */
+$("#entryBtn").click(function(){
+  var popUp = document.querySelector("#popUp");
   popUp.style.display = "block";
-}
-//Closing & Deleting new entries
-function cls() {
-  var popUp = document.getElementById("popUp");
-  popUp.style.display = "none";
-}
-//Title Entry Function
-function title() {
-  var x = document.getElementById("titleEntry").innerHTML;
-  document.getElementById("popTitle").innerHTML = x;
-}
-//New Entry function
-const addNewEntry = document.querySelector("new-call");
-addNewEntry.addEventListener("click", newEntry());
-function newEntry() {
-  const newTr = document.createElement("table-row");
-  newTr.classList.add('entry');
-  document.getElementById("titleBar").appendChild(newTr);
-}
+
+  if (!addEntry) {
+    addEntry = [
+      {
+        title: [],
+        status: [],
+        type: [],
+        assigned: [],
+        start: "TimeRanges",
+        due: "TimeRanges",
+        priority: [],
+        hours: "TimeRanges",
+        progress: [],
+      }
+    ]
+  };
+  console.log(addEntry);
+});
+
+
+/** DATEPICKER */
+// $("#calendar").datepicker({
+//   minDate: 0
+// });
+
+$(function() {
+  $("#calendar").datepicker();
+});
